197f0ee6a792d688f9b1d47cccc33ccf1ae892fc
Sintayehu G. Fantaye <131191039+sfantaye@users.noreply.github.com>
2025-04-16 11:47:19 +0300

    Update migrate_and_test.py

--- scripts/django/migrate_and_test.py
+++ scripts/django/migrate_and_test.py
@@ -1,1 +1,10 @@
+import subprocess
 
+def run():
+    print("Applying migrations...")
+    subprocess.run(["python", "manage.py", "migrate"])
+    print("Running tests...")
+    subprocess.run(["pytest"])
+
+if __name__ == '__main__':
+    run()
