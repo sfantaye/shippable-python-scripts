d3b6cee7262b1d85c3ab1aaa281ef35124484493
Sintayehu G. Fantaye <131191039+sfantaye@users.noreply.github.com>
2025-04-16 12:38:49 +0300

    Update test_backup_restore.py

--- tests/django/test_backup_restore.py
+++ tests/django/test_backup_restore.py
@@ -1,1 +1,16 @@
+import subprocess
+import pytest
+import os
+from scripts.django import backup_db, restore_db
 
+def test_backup(monkeypatch):
+    monkeypatch.setenv("DB_NAME", "test_db")
+    monkeypatch.setenv("DB_USER", "test")
+    monkeypatch.setattr(subprocess, "run", lambda x, check: True)
+    backup_db.backup_db()
+
+def test_restore(monkeypatch):
+    monkeypatch.setenv("DB_NAME", "test_db")
+    monkeypatch.setenv("DB_USER", "test")
+    monkeypatch.setattr(subprocess, "run", lambda x, check: True)
+    restore_db.restore_db("backup.sql")
